d1a3940cc00c8603d8bf924f11fdda613a2c67f7
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-02 21:53:54 +0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,88 @@
+let moveLeft = false;
+let moveRight = false;
+let jump = false;
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 500 },
+      debug: true
+    }
+  },
+  scene: {
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+let player;
+let cursors;
+function create() {
+  // ground
+  const ground = this.add.rectangle(400, 580, 800, 40, 0x00ff00);
+  this.physics.add.existing(ground, true); // static
+
+  // player
+  player = this.add.rectangle(100, 450, 40, 40, 0xff0000);
+  this.physics.add.existing(player);
+
+  player.body.setCollideWorldBounds(true);
+
+  // collision
+  this.physics.add.collider(player, ground);
+
+  // keyboard input
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // LEFT button
+const leftBtn = this.add.text(50, 500, "⬅️")
+  .setFontSize(40)
+  .setInteractive();
+
+// RIGHT button
+const rightBtn = this.add.text(120, 500, "➡️")
+  .setFontSize(40)
+  .setInteractive();
+
+// JUMP button
+const jumpBtn = this.add.text(700, 500, "⬆️")
+  .setFontSize(40)
+  .setInteractive();
+}
+// LEFT
+leftBtn.on("pointerdown", () => moveLeft = true);
+leftBtn.on("pointerup", () => moveLeft = false);
+leftBtn.on("pointerout", () => moveLeft = false);
+
+// RIGHT
+rightBtn.on("pointerdown", () => moveRight = true);
+rightBtn.on("pointerup", () => moveRight = false);
+rightBtn.on("pointerout", () => moveRight = false);
+
+// JUMP
+jumpBtn.on("pointerdown", () => jump = true);
+jumpBtn.on("pointerup", () => jump = false);
+function update() {
+  // combine keyboard + touch
+  const left = cursors.left.isDown || moveLeft;
+  const right = cursors.right.isDown || moveRight;
+
+  if (left) {
+    player.body.setVelocityX(-200);
+  } else if (right) {
+    player.body.setVelocityX(200);
+  } else {
+    player.body.setVelocityX(0);
+  }
+
+  // jump
+  if ((cursors.up.isDown || jump) && player.body.touching.down) {
+    player.body.setVelocityY(-350);
+  }
+}
