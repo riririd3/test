a4f4dc90229cece1922bfb36101285152d1494eb
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-03 20:52:37 +0700

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ create() {
   const leftBtn = this.add.rectangle(70, 520, 90,90, 0x000000, 0.4)
   .setInteractive()
   .setScrollFactor(0);
-  this.add.text(60, 500, "←");
+  this.add.text(60, 500, "<").setFontSize(50);
   leftBtn.on("pointerdown", () => {
   this.player.x -= 30;
   });
