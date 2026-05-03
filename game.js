16cb4effc46cb37021aba85372c0272fd2ef6718
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-03 21:07:55 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 class ExploreScene extends Phaser.Scene {
   constructor() {
-    super("ExploreScene");
-  }
-  
-create() {
+  super("ExploreScene");
+}
+  create() {
   this.player = this.add.rectangle(100, 100, 30, 30, 0x00ff00 );
   const leftBtn = this.add.rectangle(70, 520, 90,90, 0x000000, 0.4 )
   .setInteractive()
@@ -11,9 +10,11 @@ create() {
   this.add.text(60, 500, "<").setFontSize(50);
   leftBtn.on("pointerdown", () => {
   this.player.x -= 30;
-  });}}
-  
-  const config = {
+  });
+}
+}
+
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
