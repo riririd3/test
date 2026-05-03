8172f409b36fba77169e2cd801974c4e0f0bfa99
ridnyan~ <118254936+riririd3@users.noreply.github.com>
2026-05-03 22:15:01 +0700

    Update game.js

--- game.js
+++ game.js
@@ -4,33 +4,33 @@ class ExploreScene extends Phaser.Scene {
 }
   create() {
   this.player = this.add.rectangle(100,100,30,30,0x00ff00);
-  const leftBtn = this.add.rectangle(100,this.scale.height-120,100,100,0x000000,0.2)
+  const leftBtn = this.add.rectangle(100,this.scale.height-150,100,100,0x000000,0.2)
   .setInteractive().setScrollFactor(0);
-  const leftText = this.add.text(leftBtn.x-12,leftBtn.y-25,"<")
+  const leftText = this.add.text(leftBtn.x-12,leftBtn.y-25,"⬅️")
   .setFontSize(50).setScrollFactor(0);
   leftBtn.on("pointerdown", () => {
   this.player.x -= 30;
   });
-  const rightBtn = this.add.rectangle(200,this.scale.height-120,100,100,0x000000,0.2)
+  const rightBtn = this.add.rectangle(250,this.scale.height-150,100,100,0x000000,0.2)
   .setInteractive().setScrollFactor(0);
-  const rightText = this.add.text(rightBtn.x-12,rightBtn.y-25,">")
+  const rightText = this.add.text(rightBtn.x-12,rightBtn.y-25,"➡️")
   .setFontSize(50).setScrollFactor(0);
   rightBtn.on("pointerdown", () => {
   this.player.x += 30;
   });
-  const upBtn = this.add.rectangle(150,this.scale.height-160,100,100,0x000000,0.2)
+  const upBtn = this.add.rectangle(175,this.scale.height-200,100,100,0x000000,0.2)
   .setInteractive().setScrollFactor(0);
-  const upText = this.add.text(upBtn.x-12,upBtn.y-25,"^")
+  const upText = this.add.text(upBtn.x-12,upBtn.y-25,"⬆️")
   .setFontSize(50).setScrollFactor(0);
   upBtn.on("pointerdown", () => {
-  this.player.y += 30;
+  this.player.y -= 30;
   });
-  const downBtn = this.add.rectangle(150,this.scale.height-80,100,100,0x000000,0.2)
+  const downBtn = this.add.rectangle(175,this.scale.height-100,100,100,0x000000,0.2)
   .setInteractive().setScrollFactor(0);
-  const downText = this.add.text(downBtn.x-12,downBtn.y-25,"˅")
+  const downText = this.add.text(downBtn.x-12,downBtn.y-25,"⬇️")
   .setFontSize(50).setScrollFactor(0);
   downBtn.on("pointerdown", () => {
-  this.player.y -= 30;
+  this.player.y += 30;
   });
 }
 }
